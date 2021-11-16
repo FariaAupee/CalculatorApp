863ec2dddca77ed733f63931b0a56d865db77135
FariaAupee <aupeef@gmail.com>
2021-11-17 00:01:01 +0600

    new JAVA Class added

---
+++ app/src/main/java/com/example/calculator/opy.java
@@ -0,0 +1,18 @@
+package com.example.calculator;
+
+public class opy {
+
+    int a;
+
+    public opy(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
